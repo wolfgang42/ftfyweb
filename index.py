3bbf35d478991b45b38186823d9f28f03a02efd4
Wolfgang Faust <wolfgang42@git>
2021-06-06 17:47:59 -0700

    Display explanation of decoding steps

--- index.py
+++ index.py
@@ -5,6 +5,11 @@ import ftfy
 
 jade = install(JadePlugin(template_folder=path.dirname(path.abspath(__file__))))
 
+# Bugfix for python-jade: pyjade/ext/html.py:TYPE_CODE has a typo and incorrectly declares 'elsif' instead of 'elif'
+# Upstream seems to have been abandoned so we just hack it here
+import pyjade, operator
+pyjade.ext.html.TYPE_CODE['elif'] = operator.truth
+
 DEFAULTS = {
 	'fix_entities': 'auto',
 	'remove_terminal_escapes': False,
@@ -16,6 +21,13 @@ DEFAULTS = {
 	'remove_control_chars': True,
 	'normalization': 'NFC',
 }
+
+# Get explanations for each fix
+# (this also gets docstrings for other stuff in ftfy.fixes, but we don't care about that)
+FIX_EXPLANATIONS={}
+from inspect import getmembers, isfunction
+for fix, fixfn in getmembers(ftfy.fixes, isfunction):
+	FIX_EXPLANATIONS[fix] = (fixfn.__doc__ or '').split('\n\n')[0]
 
 def paramValue(s):
 	if s == 'True':
@@ -35,9 +47,12 @@ def index():
 			options[key] = DEFAULTS[key]
 
 	if 'mojibake' in request.query:
+		decoded, explanation = ftfy.fix_and_explain(request.query.mojibake, **options, explain=True)
 		return jade.render('index.jade', **{
 			'mojibake': request.query.mojibake,
-			'decoded': ftfy.fix_text(request.query.mojibake, **options),
+			'decoded': decoded,
+			'explanation': explanation,
+			'FIX_EXPLANATIONS': FIX_EXPLANATIONS,
 			'options': options,
 			'ftfy_version': ftfy.__version__,
 			'piwik': 'ENABLE_PIWIK' in environ,
